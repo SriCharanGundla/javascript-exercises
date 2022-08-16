12e2240e8246428771b2880ced9045fb3025f4d5
Sri Charan Gundla <gundlasricharan@gmail.com>
2022-08-16 21:29:57 +0530

    Code with all tests passed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, n) {
+  let newStr = "";
+  if (n < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < n; i++) {
+      newStr += str;
+    }
+    return newStr;
+  }
 };
 
 // Do not edit below this line
