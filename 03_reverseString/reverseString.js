801b5fb26ff5b46a5dbb7bee5afc3f35e3dd22cd
Sri Charan Gundla <gundlasricharan@gmail.com>
2022-08-16 21:48:17 +0530

    Finish code with all tests passed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reverse = "";
+  let stringArray = Array.from(string);
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverse += stringArray[i];
+  }
+  return reverse;
 };
 
 // Do not edit below this line
