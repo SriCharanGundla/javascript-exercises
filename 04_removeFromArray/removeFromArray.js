4f9001b17d158abe3b91f502039f89190dbc1e91
Sri Charan Gundla <gundlasricharan@gmail.com>
2022-08-18 13:07:28 +0530

    Finished code with all test cases passed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  /*let len = arr.length;
+  const newArr = [];
+  let j = 0;
+  for (let i = 0; i < len; i++) {
+    if (arr[i] != arg1) {
+      newArr[j] = arr[i];
+      j++;
+    }
+  }*/
+  args.forEach((arg) => {
+    const index = arr.indexOf(arg);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  });
+  return arr;
 };
 
 // Do not edit below this line
