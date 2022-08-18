78d4a3afa4a36f239a104f4cbaa21b3e1f16dd14
Sri Charan Gundla <gundlasricharan@gmail.com>
2022-08-18 13:42:09 +0530

    Finished code with all test cases passed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let max, min;
+  max = a > b ? a : b;
+  min = a < b ? a : b;
+  if (min < 0 || max < 0) {
+    return "ERROR";
+  } else if (!Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR";
+  } else {
+    let sum = 0;
+    let i;
+    for (i = min; i <= max; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
