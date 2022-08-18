22ce854765bd277041a90627c0406a3e1891845e
Sri Charan Gundla <gundlasricharan@gmail.com>
2022-08-18 14:03:59 +0530

    Finished code with all test cases passed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,28 @@
-const ftoc = function() {
+const ftoc = function (temperature) {
+  let cels;
+  let temp = (temperature - 32) * (5 / 9);
+  if (Number.isInteger(temp)) {
+    cels = temp;
+  } else {
+    cels = Math.round(temp * 10) / 10;
+  }
 
+  return cels;
 };
 
-const ctof = function() {
-
+const ctof = function (temperature) {
+  let fahr;
+  let temp = 32 + temperature * (9 / 5);
+  if (Number.isInteger(temp)) {
+    fahr = temp;
+  } else {
+    fahr = Math.round(temp * 10) / 10;
+  }
+  return fahr;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
